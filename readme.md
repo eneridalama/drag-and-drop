249a605e0a413bf4bb50bc183e057adb23be11c9
Your Name <eneridalamaa@gmail.com>
2022-02-27 17:43:09 +0100

    Added node_modules and dist folder, and some additional changes

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Drag & Drop Project
 
 This is my first TypeScript Project. 
+
+### Live Preview: https://eneridalama.github.io/drag-and-drop/
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,93 @@
-//Autobind Decorator
-function autobind(
-  _: any,
-  _2: string,
-  descriptior: PropertyDescriptor
-) {
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+  return isValid;
+}
+
+// Autobind Decorator
+function autobind(_: any, _2: string, descriptior: PropertyDescriptor) {
   const originalMethod = descriptior.value;
-  const adjDescriptor: PropertyDescriptor = { 
-      configurable: true,
-       get(){
-           const boundFn = originalMethod.bind(this);
-           return boundFn;
-       },
-     };
-     return adjDescriptor;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            "project-list"
+          )! as HTMLTemplateElement;
+          this.hostElement = document.getElementById("app")! as HTMLDivElement;
+      
+          const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+          );
+          this.element = importedNode.firstElementChild as HTMLElement;
+          this.element.id = `${this.type}-projects`;
+          this.attach();
+          this.renderContent();
+        }
+
+        private renderContent() {
+            const listId = `${this.type}-projects-list`;
+            this.element.querySelector('ul')!.id = listId;
+            this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS';
+        }
+
+          private attach(){
+            this.hostElement.insertAdjacentElement("beforeend", this.element);
+          
+
+    }
 }
 
 // ProjectInput Class
@@ -51,35 +126,53 @@ class ProjectInput {
     this.attach();
   }
 
-  private gatherUserInput(): [string, string, number] | void{
+  private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    if(enteredTitle.trim().length === 0 || 
-    enteredDescription.trim().length === 0 ||
-     enteredPeople.trim().length === 0){
-         alert('Invalid input, please try again!');
-         return;
-     }else {
-         return [enteredTitle, enteredDescription, +enteredPeople];
-     }
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
   }
 
-  private clearInputs(){
-      this.titleInputElement.value = '';
-      this.descriptionInputElement.value = '';
-      this.peopleInputElement.value = '';
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
-    if(Array.isArray(userInput)){
-        const [title, desc, people] = userInput;
-        console.log(title, desc, people);
-        this.clearInputs();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
     }
   }
 
@@ -93,3 +186,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
